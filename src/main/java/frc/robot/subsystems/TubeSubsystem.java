94158995a0e42956027c38f52c14ea70e43cc236
AdminCuivreOr <168563296+AdminCuivreOr@users.noreply.github.com>
2026-02-24 16:38:59 -0500

    Tube
    add tube

---
+++ src/main/java/frc/robot/subsystems/TubeSubsystem.java
@@ -0,0 +1,30 @@
+package frc.robot.subsystems;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+
+import com.revrobotics.spark.SparkFlex;
+import com.revrobotics.spark.SparkMax; 
+import com.revrobotics.spark.SparkLowLevel.MotorType;
+
+public class tubeSubsystem extends SubsystemBase {
+
+    private final SparkFlex shooterLeft =
+            new SparkFlex(20, MotorType.kBrushless);
+
+   // private final SparkFlex shooterRight =
+    //        new SparkFlex(22, MotorType.kBrushless);
+
+    public TubeSubsystem() {
+        // Inverser un moteur pour qu'ils tournent dans le même sens
+    }
+
+    public void drive(double speed) {
+        shooterLeft.set(speed);
+        //shooterRight.set(speed);
+    }
+
+    public void stop() {
+        shooterLeft.set(0);
+       // shooterRight.set(0);
+    }
+}
