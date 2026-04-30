05a1dd60dc318effb9fe22c43f2f4f34d83efe5e
AdminCuivreOr <168563296+AdminCuivreOr@users.noreply.github.com>
2026-04-30 15:09:43 -0400

    pid profiled
    Avec le code inspiré des Huskies

--- src/main/java/frc/robot/Intake/BrasIntake/BrasIntakeSubsystem.java
+++ src/main/java/frc/robot/Intake/BrasIntake/BrasIntakeSubsystem.java
@@ -1,8 +1,10 @@
 package frc.robot.Intake.BrasIntake;
  
 import edu.wpi.first.math.MathUtil;
+import edu.wpi.first.math.controller.ArmFeedforward;
 import edu.wpi.first.math.controller.PIDController;
 import edu.wpi.first.math.controller.ProfiledPIDController;
+import edu.wpi.first.math.trajectory.TrapezoidProfile;
 import edu.wpi.first.math.trajectory.TrapezoidProfile.Constraints;
 import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.first.wpilibj.RobotState;
@@ -15,20 +17,30 @@ import com.revrobotics.spark.SparkLowLevel.MotorType;
 import frc.robot.Intake.IntakeConstants;
  
 public class BrasIntakeSubsystem extends SubsystemBase {
+
+    private final double kp = 0.1;
+    private final double maxVelocity = 0.5;
+    private final double maxAcceleration = 0.5;
+
+
     private final SparkMax m_BrasintakeMotor = new SparkMax(IntakeConstants.motorID, MotorType.kBrushless);
     private final RelativeEncoder m_BrasintakeEncoder = m_BrasintakeMotor.getEncoder();
-    private final PIDController m_BrasintakePID = new PIDController(IntakeConstants.kp, IntakeConstants.ki, IntakeConstants.kd);
+    private final ProfiledPIDController m_BrasintakePID = 
+        new ProfiledPIDController(kp,0,0,
+            new Constraints(maxVelocity, maxAcceleration));
+
+    private ArmFeedforward feedforward = new ArmFeedforward(0.0, 0.02, 0.0);
 
   // private final ProfiledPIDController m_BrasintakePID = new ProfiledPIDController(0.07, IntakeConstants.ki, IntakeConstants.kd, new Constraints(0.5, 0.5));
     
-private double setpoint;
+    private double setpoint;
     private double command;
  
     public BrasIntakeSubsystem() {
-        m_BrasintakePID.setTolerance(IntakeConstants.tolerance);
+        m_BrasintakePID.setTolerance(/*IntakeConstants.tolerance*/1);
         m_BrasintakeMotor.getEncoder().setPosition(0);
     }
- 
+
     public void resetEncoder() {
         // Resets the encoder's position to zero
         m_BrasintakeEncoder.setPosition(0);
@@ -37,14 +49,26 @@ private double setpoint;
     public double getPosition() {
         return m_BrasintakeEncoder.getPosition();
     }
- 
-    public void setPositionTarget(double sp){
-        setpoint = sp;
+
+    public double getVitesse() {
+        return m_BrasintakeEncoder.getVelocity();
+    }
+
+    public void setPositionTarget(double cible){
+        setpoint = cible;
+
+        double moteurPID = m_BrasintakePID.calculate(getPosition(), cible);
+        double moteurFF = feedforward.calculate(getPosition(), m_BrasintakePID.getSetpoint().velocity);
+
+        m_BrasintakeMotor.set(moteurPID + moteurFF);
+        SmartDashboard.putNumber("PROFILED PID FF", moteurFF + moteurPID);
     }
  
     public boolean atSetpoint(){
-        return m_BrasintakePID.atSetpoint();
+        return m_BrasintakePID.atGoal();
     }
+
+    
  
     @Override
     public void periodic() {
@@ -56,12 +80,15 @@ private double setpoint;
         SmartDashboard.putNumber("Intake_speed", MathUtil.clamp((command + IntakeConstants.kf), -IntakeConstants.maxSpeed, IntakeConstants.maxSpeed));
         SmartDashboard.putNumber("ALternate_Intakespeed", MathUtil.clamp((IntakeConstants.kf), -IntakeConstants.maxSpeed, IntakeConstants.maxSpeed));
         SmartDashboard.putNumber("intake Position (Rotations)", getPosition());
+
+
         if(RobotState.isEnabled()){
-            command = m_BrasintakePID.calculate(m_BrasintakeEncoder.getPosition(), setpoint);
+
+           
         
            
-                  
-                   m_BrasintakeMotor.set(MathUtil.clamp((command + IntakeConstants.kf), -IntakeConstants.maxSpeed, IntakeConstants.maxSpeed));
+           // m_BrasintakeMotor.set(MathUtil.
+            //clamp((command + IntakeConstants.kf), -IntakeConstants.maxSpeed, IntakeConstants.maxSpeed));
                 
                 
             }
